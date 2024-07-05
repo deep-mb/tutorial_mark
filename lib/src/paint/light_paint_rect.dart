276de05217e4a6901c164a4a89da0170e17fa736
deep-mb <119476986+deep-mb@users.noreply.github.com>
2024-07-05 13:37:06 +0530

    Update light_paint_rect.dart

--- lib/src/paint/light_paint_rect.dart
+++ lib/src/paint/light_paint_rect.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:tutorial_coach_mark/src/clipper/rect_clipper.dart';
@@ -12,6 +13,9 @@ class LightPaintRect extends CustomPainter {
   final double offset;
   final double radius;
   final BorderSide? borderSide;
+  final Color shadowBorderColor;  // New parameter for shadow color
+  final double shadowOpacity;  // New parameter for shadow opacity
+  final double shadowSpreadRadius;  // New parameter for shadow spread radius
 
   LightPaintRect({
     required this.progress,
@@ -21,15 +25,18 @@ class LightPaintRect extends CustomPainter {
     this.offset = 10,
     this.radius = 10,
     this.borderSide,
+    this.shadowBorderColor = const Color(0xffCFF406),  // Default shadow color
+    this.shadowOpacity = 0.5,  // Default shadow opacity
+    this.shadowSpreadRadius = 4,  // Default shadow spread radius
   }) : assert(opacityShadow >= 0 && opacityShadow <= 1);
 
   static Path _drawJustHole(
-    Size canvasSize,
-    double x,
-    double y,
-    double w,
-    double h,
-  ) {
+      Size canvasSize,
+      double x,
+      double y,
+      double w,
+      double h,
+      ) {
     return Path()
       ..moveTo(x + w, y)
       ..lineTo(x + w, y + h)
@@ -39,13 +46,13 @@ class LightPaintRect extends CustomPainter {
   }
 
   static Path _drawJustRHole(
-    Size canvasSize,
-    double x,
-    double y,
-    double w,
-    double h,
-    double radius,
-  ) {
+      Size canvasSize,
+      double x,
+      double y,
+      double w,
+      double h,
+      double radius,
+      ) {
     double diameter = radius * 2;
 
     return Path()
@@ -87,37 +94,43 @@ class LightPaintRect extends CustomPainter {
         target.getBiggerSpaceBorder(size);
 
     double x = -maxSize / 2 * (1 - progress) + target.offset.dx - offset / 2;
-
     double y = -maxSize / 2 * (1 - progress) + target.offset.dy - offset / 2;
-
     double w = maxSize * (1 - progress) + target.size.width + offset;
-
     double h = maxSize * (1 - progress) + target.size.height + offset;
 
+    Path path = radius > 0
+        ? RectClipper.rRectHolePath(size, x, y, w, h, radius)
+        : RectClipper.rectHolePath(size, x, y, w, h);
+
     canvas.drawPath(
-      radius > 0
-          ? RectClipper.rRectHolePath(size, x, y, w, h, radius)
-          : RectClipper.rectHolePath(size, x, y, w, h),
+      path,
       Paint()
         ..style = PaintingStyle.fill
-        ..color = colorShadow.withOpacity(opacityShadow)
-        ..strokeWidth = 4,
+        ..color = colorShadow.withOpacity(opacityShadow),
     );
-    if (borderSide != null && borderSide?.style != BorderStyle.none) {
+
+    // Drawing the shadow effect on the border
+    if (borderSide != null && borderSide!.style != BorderStyle.none) {
+      Paint shadowPaint = Paint()
+        ..style = PaintingStyle.stroke
+        ..color = shadowBorderColor.withOpacity(shadowOpacity)
+        ..strokeWidth = borderSide!.width + shadowSpreadRadius
+        ..maskFilter = MaskFilter.blur(BlurStyle.normal, shadowSpreadRadius);
+
       canvas.drawPath(
         radius > 0
             ? _drawJustRHole(size, x, y, w, h, radius)
             : _drawJustHole(size, x, y, w, h),
-        Paint()
-          ..style = PaintingStyle.stroke
-          ..color = borderSide!.color
-          ..strokeWidth = borderSide!.width,
+        shadowPaint,
       );
     }
   }
 
   @override
   bool shouldRepaint(LightPaintRect oldDelegate) {
-    return oldDelegate.progress != progress;
+    return oldDelegate.progress != progress ||
+        oldDelegate.shadowBorderColor != shadowBorderColor ||
+        oldDelegate.shadowOpacity != shadowOpacity ||
+        oldDelegate.shadowSpreadRadius != shadowSpreadRadius;
   }
 }
