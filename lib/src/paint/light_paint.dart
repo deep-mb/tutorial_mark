495cd8f3755d4c37186a5ecd9741d3049436c73e
deep-mb <119476986+deep-mb@users.noreply.github.com>
2024-07-05 13:36:44 +0530

    Update light_paint.dart

--- lib/src/paint/light_paint.dart
+++ lib/src/paint/light_paint.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:tutorial_coach_mark/src/clipper/circle_clipper.dart';
 
@@ -10,59 +9,62 @@ class LightPaint extends CustomPainter {
   final Color colorShadow;
   final double opacityShadow;
   final BorderSide? borderSide;
+  final Color shadowBorderColor;  // New parameter for shadow color
+  final double shadowOpacity;  // New parameter for shadow opacity
+  final double shadowSpreadRadius;  // New parameter for shadow spread radius
 
   LightPaint(
-    this.progress,
-    this.positioned,
-    this.sizeCircle, {
-    this.colorShadow = Colors.black,
-    this.opacityShadow = 0.8,
-    this.borderSide,
-  }) : assert(opacityShadow >= 0 && opacityShadow <= 1);
+      this.progress,
+      this.positioned,
+      this.sizeCircle, {
+        this.colorShadow = Colors.black,
+        this.opacityShadow = 0.8,
+        this.borderSide,
+        this.shadowBorderColor = const Color(0xffCFF406),  // Default shadow color
+        this.shadowOpacity = 0.5,  // Default shadow opacity
+        this.shadowSpreadRadius = 4,  // Default shadow spread radius
+      }) : assert(opacityShadow >= 0 && opacityShadow <= 1);
 
   @override
   void paint(Canvas canvas, Size size) {
     if (positioned == Offset.zero) return;
+
     var maxSize = max(size.width, size.height);
-
     double radius = maxSize * (1 - progress) + sizeCircle;
-
     final circleHole = CircleClipper.circleHolePath(size, positioned, radius);
 
-    final justCircleHole = Path()
-      ..moveTo(positioned.dx - radius, positioned.dy)
-      ..arcTo(
-        Rect.fromCircle(center: positioned, radius: radius),
-        pi,
-        pi,
-        false,
-      )
-      ..arcTo(
-        Rect.fromCircle(center: positioned, radius: radius),
-        0,
-        pi,
-        false,
-      )
-      ..close();
-
+    // Paint the shadow-filled circle
     canvas.drawPath(
       circleHole,
       Paint()
         ..style = PaintingStyle.fill
         ..color = colorShadow.withOpacity(opacityShadow),
     );
+
+    // Paint the circle with border and optional shadow
     if (borderSide != null && borderSide?.style != BorderStyle.none) {
+      Paint borderPaint = Paint()
+        ..style = PaintingStyle.stroke
+        ..color = borderSide!.color.withOpacity(opacityShadow)
+        ..strokeWidth = borderSide!.width;
+
+      // Adding shadow to the border if enabled
+      if (shadowSpreadRadius > 0) {
+        borderPaint..maskFilter = MaskFilter.blur(BlurStyle.normal, shadowSpreadRadius);
+      }
+
       canvas.drawPath(
-          justCircleHole,
-          Paint()
-            ..style = PaintingStyle.stroke
-            ..color = borderSide!.color
-            ..strokeWidth = borderSide!.width);
+        circleHole,
+        borderPaint,
+      );
     }
   }
 
   @override
   bool shouldRepaint(LightPaint oldDelegate) {
-    return oldDelegate.progress != progress;
+    return oldDelegate.progress != progress ||
+        oldDelegate.shadowBorderColor != shadowBorderColor ||
+        oldDelegate.shadowOpacity != shadowOpacity ||
+        oldDelegate.shadowSpreadRadius != shadowSpreadRadius;
   }
 }
